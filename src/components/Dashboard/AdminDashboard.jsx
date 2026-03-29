10092af1e31cfa6795bc10d82a59b83debb7b214
Naymur000 <nayeemur000@gmail.com>
2026-03-29 23:45:15 +0600

    3rd

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,27 +1,66 @@
-import React from 'react';
-import Header from '../others/Header';
+import React from "react";
+import Header from "../others/Header";
 
 const AdminDashboard = () => {
-    return (
-        <div className='h-screen w-screen container mx-auto'>
-            <Header></Header>
+  return (
+    <div className="h-screen w-screen container mx-auto">
+      <Header></Header>
 
+      <div>
+        <form className="p-10 flex flex-wrap w-full bg-gray-900 items-start justify-between">
+          <div className="py-2">
             <div>
-                <form >
-                    <h3>Task Title</h3>
-                    <input type="text" placeholder='Make a UI design'/>
-                    <h3>Description</h3>
-                    <textarea name="" id="" cols={30} rows={10}></textarea>
-                    <h3>Date</h3>
-                    <input type="date" />
-                    <h3>Assign to</h3>
-                    <input type="text" placeholder='employee name' />
-                    <h3>Category</h3>
-                    <input type="text" placeholder='design, dev, etc'/>
-                </form>
+              <h3>Task Title</h3>
+              <input
+                className="bg-gray-400 text-black font-black px-10 py-1 border rounded-md mb-5"
+                type="text"
+                placeholder="Make a UI design"
+              />
             </div>
-        </div>
-    );
+            <div>
+              <h3>Date</h3>
+              <input
+                className="bg-gray-400 text-black font-black px-10 py-1 border rounded-md mb-5"
+                type="date"
+              />
+            </div>
+            <div>
+              <h3>Assign to</h3>
+              <input
+                className="bg-gray-400 text-black font-black px-10 py-1 border rounded-md mb-5"
+                type="text"
+                placeholder="employee name"
+              />
+            </div>
+            <div>
+              <h3>Category</h3>
+              <input
+                className="bg-gray-400 text-black font-black px-10 py-1 border rounded-md mb-5"
+                type="text"
+                placeholder="design, dev, etc"
+              />
+              <br />
+            </div>
+          </div>
+
+          <div>
+            <h3>Description</h3>
+            <textarea
+              className="bg-gray-400 rounded-xl text-black font-medium"
+              font-black
+              px-10
+              py-1
+              cols={60}
+              rows={10}
+            ></textarea>
+            <button className="block mx-auto mt-4 bg-orange-600 p-2 text-xl font-black rounded-md">
+              Create Task
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AdminDashboard;
