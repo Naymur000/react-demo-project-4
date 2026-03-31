76601b400b84fb28e0e184200e6d574e0e15bbfe
Naymur000 <nayeemur000@gmail.com>
2026-03-31 16:35:10 +0600

    5th

---
+++ src/components/others/AllTask.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AllTask = () => {
+    return (
+        <div className='bg-gray-900 p-5 mt-5 rounded'>
+            <div className='bg-[#ff8c00] py-2 px-4'>
+                <h3>Naymur</h3>
+                <h2>Make a UI design</h2>
+                <h1>Status</h1>
+            </div>
+        </div>
+    );
+};
+
+export default AllTask;
---
+++ src/components/others/CreateTask.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+const CreateTask = () => {
+  return (
+    <div>
+      <form className="mt-10 p-10 flex flex-wrap w-full bg-gray-900 items-start justify-between">
+        <div className="py-2">
+          <div>
+            <h3>Task Title</h3>
+            <input
+              className="bg-gray-400 text-black font-black px-10 py-1 border rounded-md mb-5"
+              type="text"
+              placeholder="Make a UI design"
+            />
+          </div>
+          <div>
+            <h3>Date</h3>
+            <input
+              className="bg-gray-400  text-black font-black px-18 py-1 border rounded-md mb-5"
+              type="date"
+            />
+          </div>
+          <div>
+            <h3>Assign to</h3>
+            <input
+              className="bg-gray-400 text-black font-black px-10 py-1 border rounded-md mb-5"
+              type="text"
+              placeholder="employee name"
+            />
+          </div>
+          <div>
+            <h3>Category</h3>
+            <input
+              className="bg-gray-400 text-black font-black px-10 py-1 border rounded-md mb-5"
+              type="text"
+              placeholder="design, dev, etc"
+            />
+            <br />
+          </div>
+        </div>
+
+        <div>
+          <h3>Description</h3>
+          <textarea
+            className="bg-gray-400 rounded-xl text-black font-medium"
+            font-black
+            px-10
+            py-1
+            cols={60}
+            rows={10}
+          ></textarea>
+          <button className="block mx-auto mt-4 bg-orange-600 p-2 text-xl font-black rounded-md">
+            Create Task
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateTask;
